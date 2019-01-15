f2b21844a553ceecc15508459201c16dae36d645
at-minhdao <minh.dao@asiantech.vn>
2019-01-15 14:52:32 +0700

    fix setting file

--- myproject/settings.py
+++ myproject/settings.py
@@ -35,9 +35,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin', 'django.contrib.auth',
-    'django.contrib.contenttypes', 'django.contrib.sessions',
-    'django.contrib.messages', 'django.contrib.staticfiles', 'myapp',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'myapp',
     'yashoes',
 ]
 
@@ -134,47 +138,48 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'yashoes.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    'DEFAULT_PERMISSION_CLASSES':
+    ('rest_framework.permissions.IsAuthenticated', ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
 }
 
 JWT_AUTH = {
     'JWT_ENCODE_HANDLER':
     'rest_framework_jwt.utils.jwt_encode_handler',
-
     'JWT_DECODE_HANDLER':
     'rest_framework_jwt.utils.jwt_decode_handler',
-
     'JWT_PAYLOAD_HANDLER':
     'rest_framework_jwt.utils.jwt_payload_handler',
-
     'JWT_PAYLOAD_GET_USER_ID_HANDLER':
     'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
-
     'JWT_RESPONSE_PAYLOAD_HANDLER':
     'rest_framework_jwt.utils.jwt_response_payload_handler',
-
-    'JWT_SECRET_KEY': SECRET_KEY,
-    'JWT_GET_USER_SECRET_KEY': None,
-    'JWT_ALGORITHM': 'HS256',
-    'JWT_VERIFY': True,
-    'JWT_VERIFY_EXPIRATION': True,
-    'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=3),
-    'JWT_AUDIENCE': 'team4+team1',
-    'JWT_ISSUER': 'team4+team1',
-
-    'JWT_ALLOW_REFRESH': True,
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
-
-    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-
+    'JWT_SECRET_KEY':
+    SECRET_KEY,
+    'JWT_GET_USER_SECRET_KEY':
+    None,
+    'JWT_ALGORITHM':
+    'HS256',
+    'JWT_VERIFY':
+    True,
+    'JWT_VERIFY_EXPIRATION':
+    True,
+    'JWT_LEEWAY':
+    0,
+    'JWT_EXPIRATION_DELTA':
+    datetime.timedelta(days=3),
+    'JWT_AUDIENCE':
+    'team4+team1',
+    'JWT_ISSUER':
+    'team4+team1',
+    'JWT_ALLOW_REFRESH':
+    True,
+    'JWT_REFRESH_EXPIRATION_DELTA':
+    datetime.timedelta(days=7),
+    'JWT_AUTH_HEADER_PREFIX':
+    'Bearer',
 }

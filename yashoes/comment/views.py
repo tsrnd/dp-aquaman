d483d9f1ad299eb5ef8dec59d81dcc244ecc2740
MBA0230P <mba0230p@MBA0230P.local>
2019-01-16 14:16:08 +0700

    post and get comments

---
+++ yashoes/comment/views.py
@@ -0,0 +1,36 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from yashoes.model.product import Product
+from yashoes.model.comment import Comment
+from yashoes.comment.serializers import PosCommentSerializer, GetCommentsSerializer
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+
+
+class CreateCommentView(APIView):
+    def post(self, request):
+        product = request.data.get("product")
+        content = request.data.get("content")
+        parent_comment = request.data.get("parent_comment")
+        data = {
+            'product': product,
+            'content': content,
+            'parent_comment': parent_comment
+        }
+        serializer = PosCommentSerializer(
+            data=data, context={"user": request.user})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetCommentView(APIView):
+    permission_classes = (AllowAny, )
+
+    def get(self, request, product_id):
+        comment = Comment.objects.filter(product=product_id).filter(
+            parent_comment=None)
+        data = GetCommentsSerializer(comment, many=True).data
+        return Response({"data": data})

5f4748ea91dcffbe764b295e67eb736b2a3b167d
at-anhngo <anh.ngo@asiantech.vn>
2019-01-17 11:42:23 +0700

    remove rest api router

--- yashoes_frontend/auth/urls.py
+++ yashoes_frontend/auth/urls.py
@@ -1,13 +1,3 @@
-# from rest_framework.routers import DefaultRouter
-# from yashoes_frontend.auth.views import DemoTemplate
-# from django.urls import path
-
-
-# router = DefaultRouter()
-# router.register(r'', DemoTemplate, basename='what')
-# urlpatterns = router.urls
-
-
 from django.urls import path
 from yashoes_frontend.auth.views import login, register
 

f180b0d01ad112db706c3a89f6a7e0d17017df4b
at-anhngo <anh.ngo@asiantech.vn>
2019-01-17 11:40:20 +0700

    implement ui for login and register

--- yashoes_frontend/auth/urls.py
+++ yashoes_frontend/auth/urls.py
@@ -1,8 +1,18 @@
-from rest_framework.routers import DefaultRouter
-from yashoes_frontend.auth.views import DemoTemplate
+# from rest_framework.routers import DefaultRouter
+# from yashoes_frontend.auth.views import DemoTemplate
+# from django.urls import path
+
+
+# router = DefaultRouter()
+# router.register(r'', DemoTemplate, basename='what')
+# urlpatterns = router.urls
+
+
 from django.urls import path
+from yashoes_frontend.auth.views import login, register
 
+urlpatterns = [
+    path('login/', login, name="login"),
+    path('register/', register, name="register"),
+]
 
-router = DefaultRouter()
-router.register(r'', DemoTemplate, basename='what')
-urlpatterns = router.urls

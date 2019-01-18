727643aef925d61aa4a38941eeea1d2953bf865f
at-anhngo <anh.ngo@asiantech.vn>
2019-01-18 08:39:33 +0700

    create ui for cart

--- yashoes_frontend/urls.py
+++ yashoes_frontend/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('yashoes_frontend.auth.urls')),
     path('', include('yashoes_frontend.products.urls')),
+    path('', include('yashoes_frontend.cart.urls')),
 ]

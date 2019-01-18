2974f7cd9f2516e4fa7ab84d8d04ee5fd124ee22
at-sonvu <son.vu@asiantech.vn>
2019-01-18 10:30:22 +0700

    api edit comment

--- yashoes/comment/views.py
+++ yashoes/comment/views.py
@@ -16,3 +16,19 @@ class CommentView(APIView):
             },
                             status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_200_OK)
+
+    def put(self, request, comment_id):
+        comment = get_object_or_404(Comment, pk=comment_id)
+        if request.user.id == comment.user.id:
+            content = request.data.get("content")
+            if content:
+                comment.content = content
+                comment.save()
+            else:
+                return Response({'error': 'Content must not empty'})
+        else:
+            return Response({
+                'error': "Can not edit comment of other user"
+            },
+                            status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_200_OK)

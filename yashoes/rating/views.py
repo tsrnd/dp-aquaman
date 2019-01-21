66b357791636304dfb64634d18060d7982d5fbef
at-sonvu <son.vu@asiantech.vn>
2019-01-21 10:20:59 +0700

    remove unused code

--- yashoes/rating/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.shortcuts import get_object_or_404
-
-
-class RatingView(APIView):
-    def post(self, request):
-        rating = request.data.get("rating")
-        product_id = request.data.get("product_id")

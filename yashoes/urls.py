af842cf22b33467e30454db82e0b120c955b6897
at-minhdao <minh.dao@asiantech.vn>
2019-01-21 14:07:32 +0700

    fix file urls

--- yashoes/urls.py
+++ yashoes/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path('products/', include('yashoes.product.urls')),
     path('user/', include('yashoes.user.urls')),
     path('', include('yashoes.transaction.urls')),
-    path('user/carts/', include('yashoes.cart.urls')),
-    path('cart/', include('yashoes.cart.urls')),
-    path('', include('yashoes.brand.urls')),
+    path('user/cart/', include('yashoes.cart.urls')),
 ]

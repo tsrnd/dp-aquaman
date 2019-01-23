4ca6d4d6267b6b41a0528cb7c9cf0c0ba1cd6494
phutran1 <justinphus@gmail.com>
2019-01-23 15:48:10 +0700

    raw query

--- yashoes/transaction/serializer.py
+++ yashoes/transaction/serializer.py
@@ -3,13 +3,7 @@ from yashoes.model.transaction import Transaction
 from yashoes.model.transaction_variant import TransactionVariant
 from yashoes.model.product import Product
 from yashoes.model.variant import Variant
-
 from yashoes.models import User
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('name',)
 
 class VariantSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,9 +16,12 @@ class TransactionVariantSerializer(serializers.ModelSerializer):
         model = TransactionVariant
         fields = ('quantity','variant')
 
-
-class TransactionSerializer(serializers.ModelSerializer):
+class TransactionSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
     transaction_id = serializers.IntegerField(source='id')
-    class Meta:
-        model = Transaction
-        fields = ('transaction_id','user_id','status','total','created_at')
+    product = serializers.CharField(source='name')
+    status =  serializers.CharField()
+    address = serializers.CharField()
+    total = serializers.IntegerField()
+    phone_number = serializers.IntegerField()
+    created_at = serializers.DateTimeField()
--- yashoes/transaction/views.py
+++ yashoes/transaction/views.py
@@ -6,14 +6,15 @@ from .serializer import TransactionVariantSerializer
 from yashoes.model.transaction import Transaction
 from yashoes.model.product import Product
 from yashoes.model.variant import Variant
-
+from django.http import JsonResponse
 from yashoes.model.transaction_variant import TransactionVariant
 from django.shortcuts import get_object_or_404
 
 class TransactionView(viewsets.ViewSet):
     def list(self, request):
         user = get_object_or_404(User, pk=request.user.id)
-        queryset = Transaction.objects.filter(user_id=user).exclude(deleted_at__isnull=False)
+        queryset = Transaction.objects.raw("SELECT product.name, transaction.* from product inner join variant on product.id = variant.product_id inner join transaction_variant on variant.id = transaction_variant.variant_id inner join transaction on transaction_variant.transaction_id = transaction.id")
+        print("phutran", queryset)
         serializer = TransactionSerializer(queryset, many=True)
         return Response(serializer.data, status=200)
 

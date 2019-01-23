142de8b8c44cefb65e1ea10a94c75466a64cf96c
phutran1 <justinphus@gmail.com>
2019-01-23 17:20:34 +0700

    remove serializer field mixin

--- yashoes/transaction/serializer.py
+++ yashoes/transaction/serializer.py
@@ -34,14 +34,7 @@ class TransactionSerializer(serializers.Serializer):
     phone_number = serializers.IntegerField()
     created_at = serializers.DateTimeField()
 
-class FieldMixin(object):
-    def get_field_names(self, *args, **kwargs):
-        field_names = self.context.get('fields', None)
-        if field_names:
-            return field_names
-        return super(FieldMixin, self).get_field_names(*args, **kwargs)
-
-class TransactionCreateSerializer(FieldMixin, serializers.ModelSerializer):
+class TransactionCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'

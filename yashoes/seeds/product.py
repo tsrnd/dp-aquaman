6cfc45529c4d4d3a953e449c0fdf575d460b1325
Hoa Hoang T <hoa.hoang@asiantech.vn>
2019-01-29 17:28:00 +0700

    update brand for shoes

--- yashoes/seeds/product.py
+++ yashoes/seeds/product.py
@@ -6,56 +6,56 @@ CONST_PRODUCT_LIST = [
         "name": "Neo",
         "description": "nice",
         "rate": 4,
-        "brand_id": 1
+        "brand_id": 2
     },
     {
         "id": 2,
         "name": "Ultra Boost",
         "description": "nice",
         "rate": 5,
-        "brand_id": 1
+        "brand_id": 2
     },
     {
         "id": 3,
         "name": "Supperstar",
         "description": "nice",
         "rate": 3,
-        "brand_id": 1
+        "brand_id": 2
     },
     {
         "id": 4,
         "name": "Stan Smith",
         "description": "nice",
         "rate": 5,
-        "brand_id": 1
+        "brand_id": 2
     },
     {
         "id": 5,
         "name": "Air Jordan 1",
         "description": "nice",
         "rate": 4,
-        "brand_id": 2
+        "brand_id": 1
     },
     {
         "id": 6,
         "name": "Dual Fusion Run 3",
         "description": "nice",
         "rate": 5,
-        "brand_id": 2
+        "brand_id": 1
     },
     {
         "id": 7,
         "name": "Pernix Mens Shoes Black",
         "description": "nice",
         "rate": 3,
-        "brand_id": 2
+        "brand_id": 1
     },
     {
         "id": 8,
         "name": "Air Zoom Pegasus",
         "description": "nice",
         "rate": 5,
-        "brand_id": 2
+        "brand_id": 1
     }
 ]
 

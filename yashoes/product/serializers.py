b7ff3acef88e66a7a59c8eec4184663fd69313e2
at-sonvu <son.vu@asiantech.vn>
2019-01-30 11:46:59 +0700

    handle error, format datetime comment

--- yashoes/product/serializers.py
+++ yashoes/product/serializers.py
@@ -36,7 +36,7 @@ class ListProductSerializer(serializers.Serializer):
 
 class SubCommentSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
-    created_at = serializers.DateTimeField(format='%H:%M %d %b %Y')
+    created_at = serializers.DateTimeField(format='%d %b %Y - %H:%M')
     user_image = serializers.CharField(source='user.image_profile_url')
 
     class Meta:
@@ -47,7 +47,7 @@ class SubCommentSerializer(serializers.ModelSerializer):
 class GetCommentsSerializer(serializers.ModelSerializer):
     comments = serializers.SerializerMethodField('query_comments')
     username = serializers.CharField(source='user.username')
-    created_at = serializers.DateTimeField(format='%H:%M %d %b %Y')
+    created_at = serializers.DateTimeField(format='%d %b %Y - %H:%M')
     user_image = serializers.CharField(
         source='user.image_profile_url', allow_null=True)
 

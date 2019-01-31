9eaf9e99058520eee28c20674cef0f64d2a80a95
phutran1 <justinphus@gmail.com>
2019-01-31 15:11:09 +0700

    remove redundant code

--- yashoes/remind/serializers.py
+++ yashoes/remind/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
 from yashoes.models import User
 from yashoes.model.remind import Remind
-from yashoes.model.variant import Variant
 
-
-class VariantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Variant
-        fields = '__all__'
 
 class RemindSerializer(serializers.ModelSerializer):
     class Meta:
